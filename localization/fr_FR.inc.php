250942a49350ddc277ce54f7a23f2a7d6565ea08
Bennet Becker <bbecker@pks.mpg.de>
2026-08-19 12:47:04 +0200

    add french translation

---
+++ localization/fr_FR.inc.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Copyright (c) 2026 bbecker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Machine Translated using Qwen 3 30B A3B Instruct 2507
+
+$labels["enable_in_compose_view"] = "Activer le fusion de courrier dans la vue rédaction";
+$labels["send_drafts"] = "Envoyer les brouillons";
+$labels["send_drafts_tooltip"] = "Envoyer tous les brouillons du dossier actuel ou des résultats de recherche";
+$labels["send_selected"] = "Envoyer";
+$labels["send_selected_tooltip"] = "Envoyer les messages actuellement sélectionnés";
+
+$labels["mailmerge_options"] = "Options de fusion de courrier";
+
+$labels["separator"] = "Séparateur";
+$labels["comma"] = ", (Virgule)";
+$labels["semicolon"] = "; (Point-virgule)";
+$labels["pipe"] = "| (Barre verticale)";
+$labels["tab"] = "Tabulation";
+
+$labels["field_enclosure"] = "Délimiteur de champ";
+$labels["double_quotes"] = "\" (Guillemets doubles)";
+$labels["single_quotes"] = "' (Guillemets simples)";
+
+$labels["file_encoding"] = "Encodage du fichier";
+
+$labels["generation_behavior"] = "Comportement de génération";
+$labels["generate_all"] = "Générer tous";
+$labels["generate_rcpt"] = "Générer uniquement avec au moins un destinataire (À, CC ou Cci)";
+$labels["generate_to"] = "Générer uniquement avec À";
+$labels["generate_non_empty"] = "Ignorer si un champ destinataire est vide";
+
+$labels["mailmerge"] = "Fusion de courrier";
+
+$labels["save_to_folder"] = "Enregistrer dans le dossier";
+
+$labels["sent_messages"] = "messages envoyés \$success. \$fail échoués";
+$labels["saved_messages"] = "messages enregistrés avec succès dans \$folder. \$skipped messages ignorés";
+
+$labels["no_csv_selected"] = "Aucun fichier CSV n'a été sélectionné !";
+$labels["no_message_selected"] = "Aucun message n'a été sélectionné !";
+
+$labels["not_from_global_search"] = "Impossible d'envoyer des brouillons depuis la recherche globale. Veuillez limiter à un dossier uniquement.";
